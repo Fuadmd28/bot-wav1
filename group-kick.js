b68e97d819ba7aaa82c0877d537ee8f990aaab5d
Fuadmd28 <110433657+Fuadmd28@users.noreply.github.com>
2022-09-16 15:58:09 +0700

    Update and rename plugins/owner-kick.js to group-kick.js

--- group-kick.js
+++ group-kick.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, participants }) => {
 }
 handler.help = ['kick', '-'].map(v => 'o' + v + ' @user')
 handler.tags = ['owner']
-handler.command = /^(okick|o-)$/i
+handler.command = /^(kick|k)$/i
 
 handler.admin = true
 handler.group = true
